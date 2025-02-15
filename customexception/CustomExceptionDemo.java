a539575109fb90a57068b19a3812ece0fb10f2cc
iamriatandan <tandanria@gmail.com>
2025-02-15 15:42:42 +0530

    [Ria] Added . Custom Exception

--- customexception/CustomExceptionDemo.java
+++ customexception/CustomExceptionDemo.java
@@ -1,8 +1,32 @@
 package customexception;
-
+import java.util.Scanner;
+class InvalidAgeException extends Exception{
+    public InvalidAgeException(String message){
+        super(message);
+    }
+}
 public class CustomExceptionDemo {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        //Enter age by user
+        System.out.println("Enter Age");
+        int age = sc.nextInt();
+
+        validateAge(age);
     }
-    //
+    //Method to check Custom Exceptions
+    public static void validateAge(int age){
+        if(age<18){
+            try{
+                throw new InvalidAgeException(" Age must be 18 or above");
+            }
+            catch (InvalidAgeException e){
+                System.out.println("Exception " + e.getMessage());
+            }
+        }
+        else{
+            System.out.println(" Access granted!");
+        }
+    }
 }
