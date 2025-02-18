b564ba56d4124e33fe742337b6161b3017a3e26c
iamriatandan <tandanria@gmail.com>
2025-02-18 21:57:47 +0530

    [Ria] Added . Junit : Calculator

---
+++ test/java/com/day5/junit/basicproblems/calculator/CalculatorTest.java
@@ -0,0 +1,45 @@
+package com.day5.junit.basicproblems.calculator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+    @BeforeEach
+    void setUp(){
+        System.out.println("Test Started");
+    }
+
+    @AfterEach
+    void cleanUp(){
+        System.out.println("Test Executed Successfully");
+    }
+    private final Calculator calculator = new Calculator();
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, calculator.add(2, 3));
+    }
+
+    @Test
+    public void testSubtract() {
+        assertEquals(1, calculator.subtract(4, 3));
+    }
+
+    @Test
+    public void testMultiply() {
+        assertEquals(12, calculator.multiply(3, 4));
+    }
+
+    @Test
+    public void testDivide() {
+        assertEquals(2.0, calculator.divide(6, 3));
+    }
+
+    @Test
+    public void testDivideByZero() {
+        assertThrows(ArithmeticException.class, () -> calculator.divide(6, 0));
+    }
+}
