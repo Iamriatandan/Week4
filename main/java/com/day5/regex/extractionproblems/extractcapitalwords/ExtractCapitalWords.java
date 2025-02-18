6ae55856db6a83ea7fcf8bade276066e92f0e233
iamriatandan <tandanria@gmail.com>
2025-02-18 20:26:47 +0530

    [Ria] Added . Regex : Extracted Capital Words

---
+++ main/java/com/day5/regex/extractionproblems/extractcapitalwords/ExtractCapitalWords.java
@@ -0,0 +1,33 @@
+package com.day5.regex.extractionproblems.extractcapitalwords;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ExtractCapitalWords {
+    public static void main(String[] args) {
+        String text = "The Eiffel Tower is in Paris and the Statue of Liberty is in New York.";
+
+        List<String > capitalWords = extractedCapitalWords(text);
+
+        for (String word: capitalWords) {
+            System.out.println("Capital word : " + word);
+        }
+    }
+
+    //Method to extract Capital words and store it in a list
+    public static List<String> extractedCapitalWords(String text){
+        String regex = "\\b[A-Z][a-z]*\\b";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+
+        List<String >words = new ArrayList<>();
+
+        while (matcher.find()){
+            words.add(matcher.group());
+        }
+        return words;
+    }
+}
