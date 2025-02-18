dd06f6567fe631fa25f0255c93b28988dd79f2cd
iamriatandan <tandanria@gmail.com>
2025-02-18 19:37:42 +0530

    [Ria] Added . Regex : Validate a hex code

---
+++ main/java/com/day5/regex/basicregexproblems/validhexcolourcode/ValidHexCode.java
@@ -0,0 +1,18 @@
+package com.day5.regex.basicregexproblems.validhexcolourcode;
+import java.util.*;
+public class ValidHexCode {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter HexCode : " );
+        String inputCode = sc.nextLine();
+
+        boolean result = validHexCode(inputCode);
+        System.out.println(result ? "Valid" : "Invalid");
+    }
+
+    //Method to check if hexCode is valid or not Range:
+    public static boolean validHexCode(String inputCode){
+        String regex = "^#([A-Fa-f0-9]{6})$";
+         return inputCode.matches(regex);
+    }
+}
